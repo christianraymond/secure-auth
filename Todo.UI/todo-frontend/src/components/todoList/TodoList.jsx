749a65cf8e6bb1fe38a5395eb916c34318effab4
christianrayomond <Ombenisn.raymond@gmail.com>
2021-03-22 11:34:16 +0200

    user can delete delete from the list

--- Todo.UI/todo-frontend/src/components/todoList/TodoList.jsx
+++ Todo.UI/todo-frontend/src/components/todoList/TodoList.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 
 const TodoList = ({ todos, setTodos }) => {
-  return (
-    <div>
-      {todos.map((todo) => (
-        <li className="todo-item" key={todo.id}>
-          <input
-            type="text"
-            value={todo.title}
-            className="list"
-            onChange={(event) => event.preventDefault()}
-          />
-        </li>
-      ))}
-    </div>
-  );
+
+    const handleDelete = ({ id }) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
+    return (
+        <div>
+            {todos.map((todo) => (
+                <li className="todo-item" key={todo.id}>
+                    <input
+                        type="text"
+                        value={todo.title}
+                        className="list"
+                        onChange={(event) => event.preventDefault()}
+                    />
+                    <div className="action-buttons">
+                        <button className="button-complete task-button">
+                            <i className="fa fa-check-circle"></i>
+                        </button>
+                        <button className="button-edit task-button">
+                            <i className="fa fa-edit"></i>
+                        </button>
+                        <button className="button-delete task-button" onClick={() => handleDelete(todo)}>
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </div>
+                </li>
+            ))}
+        </div>
+    );
 };
 
 export default TodoList;
