1702e63bfa0a0ece749ae46fcece1191d3288ae0
christianrayomond <Ombenisn.raymond@gmail.com>
2021-03-22 12:25:19 +0200

    user can make todo item as complete or not-completed

--- Todo.UI/todo-frontend/src/components/todoList/TodoList.jsx
+++ Todo.UI/todo-frontend/src/components/todoList/TodoList.jsx
@@ -29,7 +29,7 @@ const TodoList = ({ todos, setTodos, setEditTodo }) => {
                     <input
                         type="text"
                         value={todo.title}
-                        className="list"
+                        className={`list ${todo.completed ? "complete" : ""}`}
                         onChange={(event) => event.preventDefault()}
                     />
                     <div className="action-buttons">
